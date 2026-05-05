744a490723caa0ef06b0d7b549788db38de99611
ccastrorojas23-cmd <ccastrorojas23@gmail.com>
2026-05-04 19:03:01 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+// Service Worker v3 - Auto-limpieza
+const CACHE = 'hogar-plus-v3';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => new Response('Offline', {status: 503})));
+});
